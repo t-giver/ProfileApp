715ffc8ef0dafaf465aa953d92d451669f3cca48
spark-05 <>
2024-05-16 16:41:50 +0900

    プロフィールアプリ制作中

---
+++ ProfileApp/ProfileApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProfileApp
+//
+//  Created by spark-05 on 2024/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
